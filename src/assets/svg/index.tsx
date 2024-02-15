d82b357b1ffc1d9eb2c8a6765b2d01ab18e59a98
Chinecherem Harry <chinecherempharry@gmail.com>
2024-02-15 18:04:43 +0100

    done with navbar layout for web

--- src/assets/svg/index.tsx
+++ src/assets/svg/index.tsx
@@ -4,7 +4,12 @@ import InstagramIcon from './instagram.svg'
 import FacebookIcon from './facebook.svg'
 import TwitterIcon from './twitter.svg'
 import YoutubeIcon from './youtube.svg'
+import ArrowDownIcon from './arrow-down.svg'
+import UserIcon from './user.svg'
+import MagnifierIcon from './magnifier.svg'
+import CartIcon from './cart.svg'
+import HeartIcon from './heart.svg'
 
 export {
-    PhoneIcon, MailIcon, InstagramIcon, FacebookIcon, TwitterIcon, YoutubeIcon
+    PhoneIcon, MailIcon, InstagramIcon, FacebookIcon, TwitterIcon, YoutubeIcon, ArrowDownIcon, UserIcon, MagnifierIcon, CartIcon, HeartIcon
 }
--- src/components/navigation/NavBar.tsx
+++ src/components/navigation/NavBar.tsx
@@ -1,15 +1,212 @@
 "use-client"
-import { AppBar, Box, Toolbar, Typography } from '@mui/material'
+import { ArrowDownIcon, CartIcon, HeartIcon, MagnifierIcon, UserIcon } from '@/assets'
+import { AppBar, Box, Container, IconButton, List, ListItem, ListItemButton, ListItemText, Tab, Tabs, Toolbar, Typography } from '@mui/material'
+import Link from 'next/link'
 
 
 const NavBar = () => {
     return (
-        <AppBar position="static" sx={{ boxShadow: 'none', color: '#000', height: '58px' }}>
-            <Toolbar sx={{ backgroundColor: '#fff' }}>
-                <Typography variant="h6" component="div">
-                    aGuideHub
-                </Typography>
-            </Toolbar>
+        <AppBar position="sticky" sx={{ boxShadow: 'none', backgroundColor: '#fff', color: '#000', height: '58px' }}>
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <Box sx={{ flexGrow: 1, display: "flex", alignItems: 'center', }}>
+                        <Typography variant="h3" component="div" sx={{ fontSize: '24px', fontWeight: '700', marginRight: '119px' }}>
+                            Bandage
+                        </Typography>
+                        {/* <List sx={{ width: '100%', maxWidth: 360, display: "flex", alignItems: 'center', }}>
+
+                        <ListItemButton>
+
+                            <ListItemText primary="Photos" />
+                        </ListItemButton>
+                        <ListItemButton>
+
+                            <ListItemText primary="Photos" />
+                        </ListItemButton>
+                        <ListItemButton>
+
+                            <ListItemText primary="Photos" />
+                        </ListItemButton>
+                        <ListItemButton>
+
+                            <ListItemText primary="Photos" />
+                        </ListItemButton>
+                    </List> */}
+
+                        <Tabs centered sx={{ fontSize: '14px', }} >
+                            {/* <Tab disableRipple={true}
+                                // value="home"
+                                label={<Link href='/' style={{
+                                    textDecoration: "none",
+                                    color: "#737373",
+                                    fontSize: '14px',
+                                }}>
+                                    <p>
+                                        Home
+                                    </p>
+                                </Link>
+                                }
+                            />
+                            <Tab
+                                // value="home"
+                                label={<Link href='/' style={{
+                                    textDecoration: "none",
+                                    color: "#737373",
+                                    fontSize: '14px',
+                                }}>
+                                    <p style={{
+                                        display: 'flex',
+                                        alignItems: 'center',
+
+                                    }}>
+                                        Shop  <ArrowDownIcon />
+                                    </p>
+                                </Link>
+                                }
+                            />
+                            <Tab
+                                // value="home"
+                                label={<Link href='/' style={{
+                                    textDecoration: "none",
+                                    color: "#737373",
+                                    fontSize: '14px',
+                                }}>
+                                    <p>
+                                        About
+                                    </p>
+                                </Link>
+                                }
+                            />
+                            <Tab
+                                // value="home"
+                                label={<Link href='/' style={{
+                                    textDecoration: "none",
+                                    color: "#737373",
+                                    fontSize: '14px',
+                                }}>
+                                    <p>
+                                        Blog
+                                    </p>
+                                </Link>
+                                }
+                            /> */}
+                            {/* <Tab
+                                // value="home"
+                                label={<Link href='/' style={{
+                                    textDecoration: "none",
+                                    color: "#737373",
+                                    fontSize: '14px',
+                                }}>
+                                    <p>
+                                        Contact
+                                    </p>
+                                </Link>
+                                }
+                            />
+                            <Tab
+                                // value="home"
+                                label={<Link href='/' style={{
+                                    textDecoration: "none",
+                                    color: "#737373",
+                                    fontSize: '14px',
+                                }}>
+                                    <p>
+                                        Pages
+                                    </p>
+                                </Link>
+                                }
+                            /> */}
+                        </Tabs>
+
+                        <Box sx={{ display: "flex", justifyContent: 'space-around', alignItems: 'center', width: '361px', fontWeight: '500' }}>
+
+                            <Link href='/' style={{
+                                textDecoration: "none",
+                                color: "#737373",
+                                fontSize: '14px',
+                            }}> Home </Link>
+
+                            <Link href='/' style={{
+                                textDecoration: "none",
+                                color: "#737373",
+                                fontSize: '14px',
+                                display: 'flex',
+                                alignItems: 'center'
+                            }}>
+                                <p style={{
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    marginRight: '8px'
+
+                                }}>Shop   </p><ArrowDownIcon />
+                            </Link>
+                            <Link href='/' style={{
+                                textDecoration: "none",
+                                color: "#737373",
+                                fontSize: '14px',
+                            }}>
+                                <p>
+                                    About
+                                </p>
+                            </Link>
+                            <Link href='/' style={{
+                                textDecoration: "none",
+                                color: "#737373",
+                                fontSize: '14px',
+                            }}>
+                                Blog
+                            </Link>
+                            <Link href='/' style={{
+                                textDecoration: "none",
+                                color: "#737373",
+                                fontSize: '14px',
+                            }}> Contact</Link>
+                            <Link href='/' style={{
+                                textDecoration: "none",
+                                color: "#737373",
+                                fontSize: '14px',
+                            }}> Pages </Link>
+                        </Box>
+                    </Box>
+
+                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                        <Box>
+                            <UserIcon />
+                            <Link href="/" passHref style={{
+                                textDecoration: "none",
+                                color: "#23A6F0",
+                                fontSize: '14px',
+                                fontWeight: 'bold',
+                                marginRight: '5px'
+                            }} > Login
+
+                            </Link>
+                            <span style={{ color: '#23A6F0', fontWeight: 'bold', fontSize: '14px', }} >/</span>
+                            <Link href="/" passHref style={{
+                                textDecoration: "none",
+                                color: "#23A6F0",
+                                fontSize: '14px',
+                                fontWeight: 'bold',
+                                marginLeft: '5px'
+                            }}>Register
+                            </Link>
+                        </Box>
+                        <IconButton sx={{ margin: '0 30px' }}>
+                            <MagnifierIcon />
+                        </IconButton>
+
+                        <IconButton sx={{ marginRight: '30px', color: '#23A6F0', fontWeight: '400', fontSize: '12px', }}>
+                            <CartIcon style={{ margin: '5px' }} /> 1
+                        </IconButton>
+
+                        <IconButton sx={{ color: '#23A6F0', fontWeight: '400', fontSize: '12px', }}>
+                            <HeartIcon style={{ margin: '5px' }} /> 1
+                        </IconButton>
+                    </Box>
+
+
+                </Toolbar>
+            </Container>
         </AppBar>
 
     )
