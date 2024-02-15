125e70d6bec5bb446e8389fd220d77df4e2b0e18
Chinecherem Harry <chinecherempharry@gmail.com>
2024-02-15 15:16:30 +0100

    done with implementing the nav top

--- src/components/navigation/NavTop.tsx
+++ src/components/navigation/NavTop.tsx
@@ -1,17 +1,24 @@
-import { AppBar, Box, Container, Toolbar, Typography } from '@mui/material'
+import { FacebookIcon, InstagramIcon, MailIcon, PhoneIcon, TwitterIcon, YoutubeIcon } from '@/assets'
+import { AppBar, Avatar, Box, Container, Icon, IconButton, Toolbar, Typography } from '@mui/material'
 import React from 'react'
 
 const NavTop = () => {
     return (
-        <AppBar position="static" sx={{ boxShadow: 'none', backgroundColor: '#23856D', height: '58px', display: { xs: "none", md: "flex" } }} >
+        <AppBar position="static" sx={{ boxShadow: 'none', backgroundColor: '#23856D', height: '58px', display: { xs: "none", lg: "flex" } }} >
             {/* <Box position="static" sx={{ flexGrow: 1, backgroundColor: '#23856D', color: "#fff", height: '350px' }}> */}
             <Container maxWidth="xl">
                 <Toolbar disableGutters>
-                    <Box sx={{ flexGrow: 1, display: "flex", }}>
+                    <Box sx={{ flexGrow: 1, display: "flex", alignItems: 'center', }}>
+                        <IconButton>
+                            <PhoneIcon />
+                        </IconButton>
                         <Typography variant="h6" sx={{ fontSize: '14px', marginRight: '20px' }} >
                             (225) 555-0118
                         </Typography>
-                        <Typography variant="body1" sx={{ fontSize: '14px' }} >
+                        <IconButton>
+                            <MailIcon />
+                        </IconButton>
+                        <Typography variant="h6" sx={{ fontSize: '14px' }} >
                             michelle.rivera@example.com
                         </Typography>
                     </Box>
@@ -19,11 +26,22 @@ const NavTop = () => {
                         Follow Us  and get a chance to win 80% off
                     </Typography>
 
-                    <Box sx={{ flexGrow: 1, display: "flex", }}>
-
+                    <Box sx={{ display: "flex", alignItems: 'center' }}>
                         <Typography variant="h6" sx={{ flexGrow: 1, fontSize: '14px' }} >
                             Follow Us  :
                         </Typography>
+                        <IconButton>
+                            <InstagramIcon />
+                        </IconButton>
+                        <IconButton>
+                            <YoutubeIcon />
+                        </IconButton>
+                        <IconButton>
+                            <FacebookIcon />
+                        </IconButton>
+                        <IconButton>
+                            <TwitterIcon />
+                        </IconButton>
                     </Box>
                 </Toolbar>
             </Container>
