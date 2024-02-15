77663a9a3af3891f6bd1490f71e52e45da98a3b7
Chinecherem Harry <chinecherempharry@gmail.com>
2024-02-15 18:16:48 +0100

    added images for the header grid layout

---
+++ public/images/index.tsx
@@ -0,0 +1,9 @@
+import HeaderImg1 from './header-Img1.png'
+import HeaderImg2 from './header-Img2.png'
+import HeaderImg3 from './header-Img3.png'
+import HeaderImg4 from './header-Img4.png'
+
+
+export {
+    HeaderImg1, HeaderImg2, HeaderImg3, HeaderImg4
+}
